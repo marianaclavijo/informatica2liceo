0a561e10ea0957f04d3d99631b7f9e5ae97794a8
marianaclavijo <92605048+marianaclavijo@users.noreply.github.com>
2022-03-08 16:54:28 -0500

    Add files via upload

---
+++ validacionimput/condicionalesej1/condicionales.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+let urlcaritafeliz = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_tcPC10wzqWqUU083F1VP_LjheScA0AFQcg&usqp=CAU"
+let urlcaritatriste = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpudJMG8rF6l9ScpHON00iYWWuDNglpdmQpA&usqp=CAU"
+let imgHtml = document.querySelector("#imagencarita")
+
+
+do{
+  pregunta1= Number(prompt("Ingrese un numero del 1 al 10"))
+} while (Number.isNaN(pregunta1) || 10<pregunta1 || pregunta1<0)
+
+
+do{
+  pregunta2 = Number(prompt("Ingrese otro numero difernte del 1 al 10"))   
+    } while (Number.isNaN(pregunta2) || 10<pregunta2 || pregunta2<0)
+
+
+ do{
+   pregunta3 = Number(prompt("Ingrese otro numero diferente del 1 al 10"))
+     } while (Number.isNaN(pregunta3) || 10<pregunta3 || pregunta3<0)
+     
+     
+
+
+
+
+
+let resultado = ((pregunta1+pregunta2+pregunta3)/3)
+ if (resultado>6) {
+ imgHtml.src = urlcaritafeliz
+ }else{
+     imgHtml.src = urlcaritatriste
+ }
+ 
---
+++ validacionimput/condicionalesej2/condicionales2.js
@@ -0,0 +1,42 @@
+
+
+do{
+ masa = Number(prompt("Ingrese su masa en kg"))
+} while (Number.isNaN(masa) || 10<masa || masa<0)
+
+
+do{
+ altura = Number(prompt("Ingrese su altura en m"))
+} while (Number.isNaN(altura) || 10<altura || altura<0)
+
+
+
+
+
+let imc = (masa/(altura*altura))
+
+let clasificacion
+
+if (imc <= 18.5) {
+  clasificacion = "Insuficiencia ponderal"
+} else{
+  if (imc <= 24.9){
+    clasificacion= "Intervalo normal"
+  }else{ 
+    if (imc <= 30) {
+      clasificacion = "Sobrepeso"
+    }else{
+      if (imc <= 34.9){
+        clasificacion= "Obesidad de clase I"
+      }else{
+        if (imc <= 39.9){
+          clasificacion = "Obesidad de clase II"
+        }else{
+          clasificacion = "Obesidad de clase III"
+        }
+      }
+    }
+  }
+}
+
+document.getElementById ("resultadoimc").innerHTML = "En base a su IMC, su clasificacion es: " + clasificacion
